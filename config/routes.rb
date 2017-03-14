6f0f81f1e7b0f1a432d874777eedfd655e943636
Alex Ryan <alexryan600@gmail.com>
2017-03-14 16:51:01 +0000

    Added Administration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :orders
   root 'store#index', as: 'store_index'
   resources :line_items
@@ -8,7 +16,6 @@ Rails.application.routes.draw do
   resources :users
   resources :products
   #root 'products#index'
-  root 'store#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'products#index'
 end
